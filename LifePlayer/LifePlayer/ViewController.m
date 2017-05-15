01bd91e6969453758b03aa03f08fca63371b5a3c
fanfei <hzfanfei@corp.netease.com>
2017-05-15 23:29:26 +0800

    add ip

--- LifePlayer/LifePlayer/ViewController.m
+++ LifePlayer/LifePlayer/ViewController.m
@@ -8,29 +8,70 @@
 
 #import "ViewController.h"
 #import "KxMovieViewController.h"
+#import <GCDWebUploader.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
 
 @interface ViewController ()
+
+@property (nonatomic) GCDWebUploader* webServer;
 
 @end
 
 @implementation ViewController
 
+- (NSString *)getIPAddress {
+    
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                    
+                }
+                
+            }
+            
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // start server
+    NSString* documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    self.webServer = [[GCDWebUploader alloc] initWithUploadDirectory:documentsPath];
+    [self.webServer start];
     
+    self.title = [self getIPAddress];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
     
-
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 
