ad7b50ec5d384f82464851a67715bffaf75dc225
hzfanfei <hzfanfei@corp.netease.com>
2017-05-15 19:02:14 +0800

    init project

---
+++ LifePlayer/LifePlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LifePlayer
+//
+//  Created by familymrfan on 2017/5/15.
+//  Copyright © 2017年 niuniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LifePlayer/LifePlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LifePlayer
+//
+//  Created by familymrfan on 2017/5/15.
+//  Copyright © 2017年 niuniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
