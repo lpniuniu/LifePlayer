0182db2432ef9b47f7cbae4d7d93651118c287cc
fanfei <hzfanfei@corp.netease.com>
2017-06-11 10:28:30 +0800

    去除获取缩略图方案

--- LifePlayer/LifePlayer/ViewController.m
+++ LifePlayer/LifePlayer/ViewController.m
@@ -96,9 +96,7 @@ static NSString* cellIdentifiler = @"cellIdentifiler";
         [cell.titleLabel setTextColor:[UIColor whiteColor]];
     }
     
-    NSString* documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    NSString* path = [documentsPath stringByAppendingPathComponent:[[self getFilenamelist] objectAtIndex:indexPath.row]];
-    cell.backImageView.image = [self getThumbnailImage:path];
+    cell.backImageView.image = [UIImage imageNamed:@"video_placeholder"];
     return cell;
 }
 
@@ -141,32 +139,6 @@ static NSString* cellIdentifiler = @"cellIdentifiler";
         }
         return NO;
     }];
-}
-
-- (UIImage *)getThumbnailImage:(NSString *)videoPath {
-    if (videoPath) {
-        AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:[NSURL fileURLWithPath: videoPath] options:nil];
-        AVAssetImageGenerator *gen = [[AVAssetImageGenerator alloc] initWithAsset:asset];
-        // 设定缩略图的方向
-        // 如果不设定，可能会在视频旋转90/180/270°时，获取到的缩略图是被旋转过的，而不是正向的
-        gen.appliesPreferredTrackTransform = YES;
-        // 设置图片的最大size(分辨率)
-        gen.maximumSize = CGSizeMake(300, 169);
-        CMTime time = CMTimeMakeWithSeconds(5.0, 600); //取第5秒，一秒钟600帧
-        NSError *error = nil;
-        CMTime actualTime;
-        CGImageRef image = [gen copyCGImageAtTime:time actualTime:&actualTime error:&error];
-        if (error) {
-            UIImage *placeHoldImg = [UIImage imageNamed:@"video_placeholder"];
-            return placeHoldImg;
-        }
-        UIImage *thumb = [[UIImage alloc] initWithCGImage:image];
-        CGImageRelease(image);
-        return thumb;
-    } else {
-        UIImage *placeHoldImg = [UIImage imageNamed:@"posters_default_horizontal"];
-        return placeHoldImg;
-    }
 }
 
 // 左滑删除
